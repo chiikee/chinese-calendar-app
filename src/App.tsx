dedcb6d4c334039c7e8755d02ae6f7b29642fa3d
Leong Chii Kee <chiikee+github@gmail.com>
2022-08-09 00:11:04 +0800

    next mvp

--- src/App.tsx
+++ src/App.tsx
@@ -10,25 +10,51 @@ import Stack from '@mui/material/Stack';
 import DateChinese from "./libs/DateChinese";
 
 interface BigDayProps {
-	displayDate: Date
+	dateChinese: DateChinese;
+	lessBig?: boolean;
 }
 
-function BigDay({ displayDate }: BigDayProps) {
-	const dateChinese = new DateChinese(displayDate)
-
-	return (
-		<React.Fragment>
-			<Stack direction="column" alignItems="center">
-				<Typography variant="h1">{displayDate.getDate()}</Typography>
-				<pre>{JSON.stringify(dateChinese, null, "\t")}</pre>
-			</Stack>
-		</React.Fragment>
-	)
+function BigDay({ dateChinese, lessBig = false }: BigDayProps) {
+	if (lessBig) {
+		return (
+			<React.Fragment>
+				{/* <Stack direction="column" alignItems="center"> */}
+				<Stack direction="column" alignItems="center">
+					<Typography variant="h4">{dateChinese.gregorianMonthName}</Typography>
+					<Typography variant="h2">{dateChinese.gregorianDate}</Typography>
+					<Typography variant="h6">{dateChinese.gregorianDayName}</Typography>
+					<Typography variant="subtitle2">{dateChinese.monthName}{dateChinese.dateName}</Typography>
+					<Typography variant="h6">{dateChinese.moonPhase}</Typography>
+				</Stack>
+				{/* <pre>{JSON.stringify(dateChinese, null, "\t")}</pre> */}
+				{/* </Stack> */}
+			</React.Fragment>
+		)
+	} else {
+		return (
+			<React.Fragment>
+				{/* <Stack direction="column" alignItems="center"> */}
+				<Stack direction="column" alignItems="center">
+					<Typography variant="h3">{dateChinese.gregorianMonthName}</Typography>
+					<Typography variant="h1">{dateChinese.gregorianDate}</Typography>
+					<Typography variant="h5">{dateChinese.gregorianDayName}</Typography>
+					<Typography variant="subtitle1">{dateChinese.monthName}{dateChinese.dateName}</Typography>
+					<Typography variant="h5">{dateChinese.moonPhase}</Typography>
+				</Stack>
+				{/* <pre>{JSON.stringify(dateChinese, null, "\t")}</pre> */}
+				{/* </Stack> */}
+			</React.Fragment>
+		)
+	}
 }
 
 export default function App() {
 	const [displayDate, setdisplayDate] = React.useState(new Date());
 	const defaultDate = displayDate.toISOString().slice(0, 10);
+
+	const todayChinese = new DateChinese(displayDate)
+	const nextNewOrFullMoon = new DateChinese(todayChinese.nearestNewOrFullMoon(true));
+	const prevNewOrFullMoon = new DateChinese(todayChinese.nearestNewOrFullMoon(false));
 
 	const handleDateChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
 		const inputDate = new Date(e.target.value);
@@ -40,7 +66,7 @@ export default function App() {
 	}
 
 	return (
-		<Container maxWidth="sm">
+		<Container>
 			<Stack direction="column" alignItems="center">
 				<Box sx={{ my: 4 }}>
 					<TextField
@@ -51,7 +77,15 @@ export default function App() {
 						onChange={handleDateChange}
 					/>
 				</Box>
-				<BigDay displayDate={displayDate} />
+				<Typography variant="h4">
+					{todayChinese.gregorianYear}{todayChinese.tianganName}{todayChinese.dizhiName}
+					{todayChinese.zodiac}{todayChinese.zodiacEmoji}
+				</Typography>
+				<Box paddingBottom={3}><BigDay dateChinese={todayChinese} /></Box>
+				<Stack direction="row" alignItems="center" spacing={5}>
+					<BigDay dateChinese={prevNewOrFullMoon} lessBig />
+					<BigDay dateChinese={nextNewOrFullMoon} lessBig />
+				</Stack>
 			</Stack>
 		</Container>
 	);
--- src/libs/DateChinese.ts
+++ src/libs/DateChinese.ts
@@ -7,9 +7,13 @@ import Constants from './ChineseCalendarConstants';
 // so that this object stays trim, we're not going to hold on to the instance of CalendarChinese
 
 export default class DateChinese {
+    calendarChinese = new CalendarChinese();
+
     gregorianYear: number;
     gregorianMonth: number;
     gregorianDate: number;
+    gregorianDayName: string;
+    gregorianMonthName: string;
 
     cycle: number;
     year: number;
@@ -27,13 +31,17 @@ export default class DateChinese {
     monthAlias = "";
     dateName = "";
     jieqi = "";
+    moonPhase = "";
+    zodiacEmoji = "";
 
     constructor(date: Date) {
+        const { calendarChinese } = this;
         this.gregorianDate = date.getDate();
         this.gregorianMonth = date.getMonth() + 1;
         this.gregorianYear = date.getFullYear();
+        this.gregorianDayName = Constants.DAYS[date.getDay()];
+        this.gregorianMonthName = Constants.MONTHS[date.getMonth()];
 
-        const calendarChinese = new CalendarChinese();
         calendarChinese.fromDate(date);
         const parts = calendarChinese.get();
         this.cycle = parts[0];
@@ -52,7 +60,7 @@ export default class DateChinese {
         if (this.gregorianDate === nearestSTDate.getDate() &&
             this.gregorianMonth === (nearestSTDate.getMonth() + 1) &&
             this.gregorianYear === nearestSTDate.getFullYear()) {
-            this.solarTerm = nearestSolarTerm < 1 ? nearestSolarTerm + 24 : nearestSolarTerm; 
+            this.solarTerm = nearestSolarTerm < 1 ? nearestSolarTerm + 24 : nearestSolarTerm;
         }
 
         //names
@@ -63,11 +71,40 @@ export default class DateChinese {
         this.monthAlias = Constants.LUNAR_MONTHS_ALIAS[this.month - 1];
         this.dateName = Constants.LUNAR_DATES[this.date - 1];
         this.jieqi = this.solarTerm === -1 ? "" : Constants.LUNAR_DATES[this.solarTerm - 1];
+        this.moonPhase = Constants.LUNAR_PHASES_APPROXIMATE[this.date - 1];
+        this.zodiacEmoji = Constants.ZODIAC_EMOJI[this.dizhi - 1];
     }
 
     // https://stackoverflow.com/a/40975730
     static daysIntoYear(date: Date) {
         return (Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()) - Date.UTC(date.getFullYear(), 0, 0)) / 24 / 60 / 60 / 1000;
     }
+
+    nearestNewOrFullMoon(next = true): Date {
+        const { gregorianDate, gregorianMonth, gregorianYear, calendarChinese } = this;
+        const newDate = new Date(gregorianYear, gregorianMonth - 1, gregorianDate);
+        let change = next ? 1 : -1, chineseDate: number;
+        let delta = change;
+        do {
+            newDate.setFullYear(gregorianYear);
+            newDate.setMonth(gregorianMonth - 1);
+            newDate.setDate(gregorianDate + delta);
+            calendarChinese.fromDate(newDate)
+            chineseDate = calendarChinese.get()[4]
+            delta += change;
+        } while (chineseDate !== 1 && chineseDate !== 15)
+        return newDate;
+    }
 }
 
+
+// function nextLunarEvent(dateToCheck: Date): DateChinese {
+//     const date = dateToCheck.getDate();
+//     let increment = 1, newDate: Date;
+//     do {
+//         newDate =
+//         chineseDate = new DateChinese(date);
+//     } while ()
+//     const currCD = new DateChinese(date);
+//     if (currCD.date >= 15)
+// }
